961d0ebad8a263028afea4dda6217fb8a78cde9c
ARGAUD <romain943602@gmail.com>
2024-09-03 21:09:14 -0400

    change opacity and put string into file

--- app/src/main/java/com/example/seance2/MainActivity.kt
+++ app/src/main/java/com/example/seance2/MainActivity.kt
@@ -18,7 +18,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,8 +38,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Sam!",
-                        from = "From Emma",
+                        message = stringResource(R.string.happy_birthday_sam),
+                        from = stringResource(R.string.signature_text),
                         modifier = Modifier.padding(8.dp)
                         )
                 }
@@ -74,7 +76,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     Box(modifier) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         GreetingText(
             message = message,
@@ -91,8 +95,8 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
 fun BirthdayCardPreview() {
     Seance2Theme {
         GreetingImage(
-            message = "Happy Birthday Sam!",
-            from = "From Emma"
+            message = stringResource(R.string.happy_birthday_sam),
+            from = stringResource(R.string.signature_text)
         )
     }
 }
